4e28a13d182e01068bbdcdeef455ec353e6f9014
Jason Butwell <jasonbutwell@gmail.com>
2016-10-11 17:58:03 +0100

    Enabled fullscreen/portrait mode and set permissions in manifest.

--- app/src/main/java/com/jasonbutwell/hikerswatch/MainActivity.java
+++ app/src/main/java/com/jasonbutwell/hikerswatch/MainActivity.java
@@ -1,11 +1,11 @@
 package com.jasonbutwell.hikerswatch;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
