76226dd2a38b519fa6d3114330e1b8c812a8fcaf
Jason Butwell <jasonbutwell@gmail.com>
2016-10-11 21:04:16 +0100

    Implemented code to get more location data from the location object and created more text views in XML code view to present the data to the user.

--- app/src/main/java/com/jasonbutwell/hikerswatch/MainActivity.java
+++ app/src/main/java/com/jasonbutwell/hikerswatch/MainActivity.java
@@ -4,7 +4,9 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
 import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -14,10 +16,21 @@ import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class MainActivity extends Activity implements LocationListener {
 
     private LocationManager locationManager;
     private String provider;
+
+    private Double lat;
+    private Double lon;
+    private Double alt;
+    private Float bearing;
+    private Float speed;
+    private Float accuracy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,10 +61,12 @@ public class MainActivity extends Activity implements LocationListener {
         }
 
         if (location != null) {
-            Log.i("Location", "Location Achieved");
+            Log.i( "Location", "Location Achieved" );
+
+            // call to onLocationChanged in first instance of execution from onCreate() method.
             onLocationChanged(location);
         } else {
-            Log.i("Location", "No Location");
+            Log.i( "Location", "No Location" );
         }
 
     }
@@ -79,8 +94,6 @@ public class MainActivity extends Activity implements LocationListener {
     protected void onResume() {
         super.onResume();
 
-        // 400 = 400 milli seconds update window
-        // 1 = meter distance accuracy
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -92,20 +105,55 @@ public class MainActivity extends Activity implements LocationListener {
 
             return;
         }
+
+        // 400 = 400 milli seconds update window
+        // 1 = meter distance accuracy
+
         locationManager.requestLocationUpdates(provider, 400, 1, this);
     }
 
     @Override
     public void onLocationChanged(Location location) {
+
+        // grab all the info we want from the location
+
         Double lat = location.getLatitude();
         Double lon = location.getLongitude();
+        Double alt = location.getAltitude();
+        Float bearing = location.getBearing();
+        Float speed = location.getSpeed();
+        Float accuracy = location.getAccuracy();
 
-        Log.i("Location", "LAT/LON:"+lat.toString()+","+lon.toString() );
+        // We need a geocoder to resolve the lat / long to a street address
+
+        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+        try {
+            // List of Addresses. We store the addresses we find at that lat long location in this list.
+            // We only want 1 for now.
+            List<Address> listAddresses = geocoder.getFromLocation(lat, lon, 1);
+
+            // Ensure we actually do have some locations in our list to display
+            if ( listAddresses != null && listAddresses.size() > 0 )
+                Log.i("PlaceInfo", listAddresses.get(0).toString());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Output the info we have obtained from the location to the console for now
+        Log.i("Location", "LAT/LON:"+lat.toString()+","+lon.toString());
+
+        Log.i("Latitude", String.valueOf(lat));
+        Log.i("Longitude", String.valueOf(lon));
+        Log.i("Altitude", String.valueOf(alt));
+        Log.i("Bearing", String.valueOf(bearing));
+        Log.i("Speed", String.valueOf(speed));
+        Log.i("Accuracy", String.valueOf(accuracy));
     }
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
-
     }
 
     @Override
